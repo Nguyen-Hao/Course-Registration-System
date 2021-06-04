57ac7861dc74506443a4bbd0c834e0a506ef38ec
ToTranSonBa <sonba4102@gmail.com>
2021-06-04 23:04:38 +0700

    bo sung login

--- FinalProject/login.h
+++ FinalProject/login.h
@@ -6,3 +6,33 @@ struct List_Sv // list
 };
 ListSV* Create_Node_Sv(SinhVien sv);
 void Create_List_Sv(List_Sv& dssv);
+int xulyluachon(int soluachon);
+int chon_tai_khoan();
+int xulyluachon(int soluachon)
+{
+	int luachon;
+	bool kt;
+	do
+	{
+		cout << "moi ban chon: ";
+		cin >> luachon;
+		kt = cin.fail();
+		if (kt == false && (luachon > 0 && luachon <= soluachon))
+		{
+			break;
+		}
+		cin.clear();
+		cin.ignore();
+	} while (true);
+	return luachon;
+}
+
+int chon_tai_khoan()
+{
+	cout << "ban la giao vien hay hoc sinh?" << endl;
+	cout << "1. giao vien" << endl;
+	cout << "2. hoc sinh" << endl;
+	cout << "3. thoat" << endl;
+	int a = xulyluachon(3);
+	return a;
+}
