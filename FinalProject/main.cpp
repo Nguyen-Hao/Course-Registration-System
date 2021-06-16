de46d54d039e97d888fd6347b9788b98335fbc9d
Dat-TG <dat13102k2@gmail.com>
2021-06-16 22:45:58 +0700

    change struct PhienGio

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -9,7 +9,6 @@
 #include "Menu.h"
 int main()
 {
-<<<<<<< HEAD
 	ListLop dsl;
 	List_GV dsgv;
 	dsgv.pHead = NULL;
@@ -23,23 +22,10 @@ int main()
 	Read_File_DSGV(dsgv);
 	UpdateCSV(dsl);
 	MenuSV(dsl, p, dsgv);
-	int x;
-=======
-	ListLop dsl;			// Dung xoa cai nay khi chay chuong trinh
-	dsl.n = 0;				// Dung xoa cai nay khi chay chuong trinh
-	ListSV* p = new ListSV;	// Dung xoa cai nay khi chay chuong trinh
-	//xu_ly_menu_student();
-	FixConsoleWindow();		// Dung xoa cai nay khi chay chuong trinh
-	resizeConsole(1200, 600);// Dung xoa cai nay khi chay chuong trinh
-	ReadFileClass(dsl);		// Dung xoa cai nay khi chay chuong trinh
-	ReadFileStudent(dsl);	// Dung xoa cai nay khi chay chuong trinh
-	UpdateCSV(dsl);			// Dung xoa cai nay khi chay chuong trinh
-	MenuSV(dsl, p);			// ham de chay menu
 	//ListSV* temp = DangNhapSV(dsl);
 	//cout << temp->info.Class << endl << temp->info.DateOfBirth << endl;
 	//int c = AfterLoginSV(dsl, p);
 	//int x;
->>>>>>> 491374b42da0783b0968438f10e0b073c869e24b
 	//ViewListOfCourse(x);
 	//updateCourse();
 	//UpdateStudent();
